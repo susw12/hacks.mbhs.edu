e7120d4acaf55b408188958319d508d8ce7e7c33
Sujay Swain <sujaymswain@gmail.com>
2018-09-19 14:45:32 -0400

    First push base

---
+++ mainsite/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sujay\Desktop\Git Repos\hacks.mbhs.edu\mainsite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
